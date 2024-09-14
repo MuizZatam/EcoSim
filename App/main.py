52ee59325ebc902d7ea6845b920c6bc523ff7940
MuizZatam <muizzatam110@gmail.com>
2024-09-14 23:14:04 +0530

    Define folder structure and initialize pipenv

---
+++ App/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main() -> None:
+
+    st.set_page_config(
+
+        page_title = 'Env-Sim',
+        layout='wide'
+    )
+
+    st.markdown('# Env Sim')
+
+if __name__ == '__main__':
+
+    main()
